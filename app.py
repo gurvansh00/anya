c95b7d87ec8c9ca92c5dcdf6dad1f060f49fbb06
gurvansh00 <118958451+gurvansh00@users.noreply.github.com>
2023-07-07 22:11:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-import moviepy.editor as moviepy
+import tempfile
+import subprocess
 
-video = st.file_uploader('upload your video')
+video = st.file_uploader('upload your video',type=['mp4','mpeg4','avi'])
 if video is not None:
-  clip = moviepy.VideoFileClip(video)
-  clip.write_videofile("myvideo.MOV")
-st.write('done processing')
-
-st.video('myvideo.MOV')
+  tfile = tempfile.NamedTemporaryFile(delete=False)
+  tfile.write(f.read())
+  subprocess.call(['ffmpeg','-i',tfile.name,'user.MOV'])
+  
