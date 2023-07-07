8d7f6d20f53b9eef29959cf6f7cb3b765dac764c
gurvansh00 <118958451+gurvansh00@users.noreply.github.com>
2023-07-07 21:48:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-import subprocess
 import streamlit as st
+import moviepy.editor as moviepy
 
-vid = st.file_uploader('upload file')
-if 
+video = st.file_uploader('upload your video')
+if video is not None:
+  clip = moviepy.VideoFileClip(video)
+  clip.write_videofile("myvideo.MOV")
+st.write('done processing')
+
+st.video('myvideo.MOV')
