03b9662101e582474a9a8c63db23b96cdba86a9a
gurvansh00 <118958451+gurvansh00@users.noreply.github.com>
2023-07-18 11:57:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -396,7 +396,7 @@ if video is not None:
     analyzer.analyze(tfile.name,joints)
     st.write('Overall Score: ',analyzer.score_motion())
     st.write(analyzer.give_suggestions())
-    os.remove(tfile.name)
+    os.remove('user.Mov)
     #analyzer.output_video(name = 'user', limbs = limbs, out_frame_rate = 12)
     #st.video('user.mp4')
     st.pyplot(analyzer.output_graph())
